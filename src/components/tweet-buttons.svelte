1b29fc01edc0f6a75eb545f9c96dfd37ecefd792
erixweb <erikmorenodev@gmail.com>
2023-08-28 12:18:31 +0200

    Add svelte stores

--- src/components/tweet-buttons.svelte
+++ src/components/tweet-buttons.svelte
@@ -1,16 +1,32 @@
-<script>
+<script lang="ts">
+    import { posts } from "../store";
     import Bookmark from "./icons/bookmark.svelte";
     import Like from "./icons/like.svelte";
     import Retweet from "./icons/retweet.svelte";
 
+    export let id: string
+
     let liked = false
     let retweeted = false
     let bookmarked = false
+    let existingPosts: Array<Post> = []
+
+    posts.subscribe(value => {
+        existingPosts = value
+    })
+
+    const currentPost: Post | null = existingPosts.find((post: Post) => post.id === id) || null
+
+
+    const post: Post = {
+        id: id,
+        liked: currentPost?.liked ?? false
+    }
 </script>
 
 <div class="flex gap-[20px] mt-[20px]">
-    <button on:click={() => liked = !liked}>
-        <Like liked={liked} />
+    <button on:click={() => post.liked = !post.liked}>
+        <Like liked={post.liked} />
     </button>
     <button on:click={() => retweeted = !retweeted}>
         <Retweet retweeted={retweeted} />
